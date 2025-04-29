004da2ad895dcb8ea4d1b0fc7533d35b7ca1c1cf
Solidity Programer <routeres@gmail.com>
2025-04-29 00:27:45 +0000

    Move ssh keys to the correct folder and test ssh connection

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,19 +1,26 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
-{pkgs}: {
+{ pkgs }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.11"; # or "unstable"
+
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.openssh
     pkgs.nodejs_20
+    pkgs.git # Opcional si usas git
   ];
+
   # Sets environment variables in the workspace
   env = {};
+
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "ms-vscode.vscode-typescript-next"
+      "esbenp.prettier-vscode"
     ];
+
     workspace = {
       onCreate = {
         default.openFiles = [
@@ -21,6 +28,7 @@
         ];
       };
     };
+
     # Enable previews and customize configuration
     previews = {
       enable = true;
